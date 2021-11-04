ad67547328b8b80658802d5b11335227d16af33e
Mens1s <ahmet_yigit_02@hotmail.com>
2021-11-04 23:50:22 +0300

    'master'

--- Exercise/Python/endKodluyoruz/ConsecutiveStrings.py
+++ Exercise/Python/endKodluyoruz/ConsecutiveStrings.py
@@ -1,4 +1,5 @@
-# # # 1- Bir listeyi düzleştiren (flatten) fonksiyon yazın. Elemanları birden çok katmanlı listelerden ([[3],2] gibi) oluşabileceği gibi, non-scalar verilerden de oluşabilir. Örnek olarak:
+# # # 1- Bir listeyi düzleştiren (flatten) fonksiyon yazın. Elemanları birden çok katmanlı listelerden ([[3],2] gibi) oluşabileceği gibi, 
+# non-scalar verilerden de oluşabilir. Örnek olarak:
 
 # # input: [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 
@@ -10,7 +11,28 @@
 
 # output: [[[7, 6, 5], [4, 3], [2, 1]]
 
-def flatten():
-    liste = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
-    print([b for a in liste for b in a])
-flatten()
+# class homework():
+#     def __init__(self,liste):
+#         self.result = []
+#         self.liste = liste
+
+#     def flatten(self):
+#         for a in self.liste:
+#             if isinstance(a,list):
+#                 homework.flatten(a)
+#             else:
+#                 self.result.append(a)
+#         print(self.result)
+# liste = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+# homework.flatten(liste)
+result = []
+def flatten(liste):
+    for a in liste:
+        if isinstance(a,list):
+            flatten(a)
+        else:
+            result.append(a)
+            
+liste = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+flatten(liste)
+print(result)
